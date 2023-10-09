db165e1ff13efc4baba9b5fc1b17fc42a618093a
Todor Mladenov <144213026+todormladenov@users.noreply.github.com>
2023-10-09 11:44:53 +0200

    Add files via upload

--- JS-Advanced-Exercises/04.DOM Introduction/07. Hell's Kitchen/app.js
+++ JS-Advanced-Exercises/04.DOM Introduction/07. Hell's Kitchen/app.js
@@ -2,34 +2,71 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
    let textAreaElement = document.querySelectorAll('#inputs textarea');
 
-   function onClick () {
+   function onClick() {
       textAreaArr = JSON.parse(textAreaElement[0].value);
 
-      let averageSalary = 0;
       let restaurants = {};
 
       textAreaArr.forEach(restaurant => {
          let [currRestaurant, workers] = restaurant.split(' - ');
-         
-         let workersArr = workers.split(', ');
 
-         for (let el of workersArr) {
-
-            let tokens = el.split(' ');
-            let worker = tokens[0];
-            let salary = Number(tokens[1]);
-         
-            if (!restaurants.hasOwnProperty(currRestaurant)) {
-               restaurants.restaurantName = 
-            } else {
-               found[worker] += salary;
-            }
-
-            debugger
-            
-            
+         if (!restaurants.hasOwnProperty(currRestaurant)) {
+            restaurants[currRestaurant] = {
+               averageSalary: clacAverageSalary(workers),
+               bestSalary: clacBestSalary(workers),
+               workers: workers
+            };
+         } else {
+            restaurants[currRestaurant].workers = addWorkers(restaurants[currRestaurant][workers], workers);
+            restaurants[currRestaurant].averageSalary = clacAverageSalary(restaurants[currRestaurant][workers]);
+            restaurants[currRestaurant].bestSalary = clacBestSalary(restaurants[currRestaurant][workers]);
          }
       });
+
+      function clacAverageSalary(workers) {
+         let workersArr = workers.split(', ');
+         let averageSalary = 0;
+
+         for (let el of workersArr) {
+            let [worker, salary] = el.split(' ');
+            averageSalary += Number(salary);
+         }
+         return averageSalary / workersArr.length;
+      }
+
+      function clacBestSalary(workers) {
+         let workersArr = workers.split(', ');
+         let bestSalary = 0;
+
+         for (let el of workersArr) {
+            let [worker, salary] = el.split(' ');
+            if (salary > bestSalary) {
+               bestSalary = el;
+            }
+         }
+         return bestSalary
+      }
+
+      function addWorkers(oldWorkers, newWorkers) {
+         return oldWorkers.concat(newWorkers);
+      }
+
+      let theBestRestaurantsSorted = Object.entries(restaurants).sort((a, b) => b[1].averageSalary - a[1].averageSalary);
+      let theBestRestaurant = theBestRestaurantsSorted[0];
+
+      let theBestRestaurantWorkersSorted = theBestRestaurant[1].workers.split(', ')
+         .sort((a, b) => b.split(' ')[1] - a.split(' ')[1]);
+      let theBestWorkerSalary = Number(theBestRestaurantWorkersSorted[0].split(' ')[1]);     
+
+      let theBestRestaurantsPElement = document.querySelector('div[id=bestRestaurant] p');
+      theBestRestaurantsPElement.textContent = `Name: ${theBestRestaurantsSorted[0][0]} Average Salary: ${theBestRestaurantsSorted[0][1].averageSalary.toFixed(2)} Best Salary: ${theBestWorkerSalary.toFixed(2)}`;
+
+      let bestWorkersPElement = document.querySelector('div[id=workers] p');
+      bestWorkersPElement.textContent = ``;
       
+      for (let workerInfo of theBestRestaurantWorkersSorted) {
+         let [name, money] = workerInfo.split(' ');
+         bestWorkersPElement.textContent += `Name: ${name} With Salary: ${money} `;
+      }
    }
 }
--- JS-Advanced-Exercises/04.DOM Introduction/08. Generate Report/report.js
+++ JS-Advanced-Exercises/04.DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,53 @@
 function generateReport() {
-    //TODO
+    let checkboxElements = document.querySelectorAll('input[type=checkbox]');
+    let employeeElement = checkboxElements[0];
+    let deparmentElement = checkboxElements[1];
+    let statusElement = checkboxElements[2];
+    let dateHiredElement = checkboxElements[3];
+    let benefitsElement = checkboxElements[4];
+    let salaryElement = checkboxElements[5];
+    let ratingElement = checkboxElements[6];
+
+    let trElements = document.querySelectorAll('tbody tr');
+
+    let outputElement = document.getElementById('output');
+    let result = [];
+
+    for (let el of trElements) {
+        let tdElements = el.children;
+
+        let reportObj = {}
+
+        if (employeeElement.checked) {
+            reportObj.employee = tdElements[0].textContent;
+        }
+
+        if (deparmentElement.checked) {
+            reportObj.deparment = tdElements[1].textContent;
+        }
+
+        if (statusElement.checked) {
+            reportObj.status = tdElements[2].textContent;
+        }
+
+        if (dateHiredElement.checked) {
+            reportObj.dateHired = tdElements[3].textContent;
+        }
+
+        if (benefitsElement.checked) {
+            reportObj.benefits = tdElements[4].textContent;
+        }
+
+        if (salaryElement.checked) {
+            reportObj.salary = tdElements[5].textContent;
+        }
+
+        if (ratingElement.checked) {
+            reportObj.rating = tdElements[6].textContent;
+        }
+
+        result.push(reportObj);
+    }
+
+    outputElement.textContent = JSON.stringify(result);
 }
